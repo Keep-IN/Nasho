c76188122967761c49fd5475e01cb231f8b77e73
Kevin <kevinraihanr45@gmail.com>
2024-07-31 19:55:10 +0700

    Success get quiz

--- app/src/main/java/com/nasho/features/quiz/quizGrade/QuizGrade.kt
+++ app/src/main/java/com/nasho/features/quiz/quizGrade/QuizGrade.kt
@@ -3,16 +3,24 @@ package com.nasho.features.quiz.quizGrade
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.viewModelScope
+import com.core.data.network.Result
 import com.nasho.R
 import com.nasho.databinding.ActivityQuizGradeBinding
+import com.nasho.features.quiz.QuizViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class QuizGrade : AppCompatActivity() {
     private lateinit var binding: ActivityQuizGradeBinding
+    private val viewModel: QuizViewModel by viewModels()
+    private var idQuiz: String = "a2011811-212a-4d15-a415-3fb7e437b432"
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityQuizGradeBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -25,6 +33,25 @@ class QuizGrade : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    private fun getQuizGrade(id: String){
+        viewModel.viewModelScope.launch(Dispatchers.Main){
+            viewModel.getQuizGrade(id).observe(this@QuizGrade){
+                when(it){
+                    is Result.Success -> {
+
+                    }
+                    is Result.Error -> {
+
+
+                    }
+                    else -> {
+
+                    }
+                }
+            }
         }
     }
 }
